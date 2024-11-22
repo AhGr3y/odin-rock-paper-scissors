bcfe374ab2c6eb0c09aeba3179c6fa8b3f780f89
ahgr3y <leehh97@gmail.com>
2024-11-22 14:35:23 +0800

    Add playGame and playRound

--- javascript.js
+++ javascript.js
@@ -1,10 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -29,48 +23,73 @@ function playRound(humanChoice, computerChoice) {
             switch (computerChoice) {
                 case "rock":
                     console.log("Draw! Rock ties with Rock.");
-                    break;
+                    return 2;
                 case "paper":
                     console.log("You lose! Paper beats Rock.");
-                    computerScore++;
-                    break;
+                    return 0;
                 case "scissors":
                     console.log("You win! Rock beats Scissors.");
-                    humanScore++;
-                    break;
+                    return 1;
             }
             break;
         case "paper":
             switch (computerChoice) {
                 case "rock":
                     console.log("You win! Paper beats Rock.");
-                    humanScore++;
-                    break;
+                    return 1;
                 case "paper":
                     console.log("Draw! Paper ties with Paper.");
-                    break;
+                    return 2;
                 case "scissors":
                     console.log("You lose! Scissors beats Paper.");
-                    computerScore++;
-                    break;
+                    return 0;
             }
             break;
         case "scissors":
             switch (computerChoice) {
                 case "rock":
                     console.log("You lose! Rock beats Scissors.");
-                    computerScore++;
-                    break;
+                    return 0;
                 case "paper":
                     console.log("You win! Scissors beats Paper.");
-                    humanScore++;
-                    break;
+                    return 1;
                 case "scissors":
                     console.log("Draw! Scissors ties with Scissors.");
-                    break;
+                    return 2;
             }
             break;
         default:
-            console.log("Invalid input!");
+            console.log("Invalid input! Try again.");
+            return -1;
     }
+}
+
+function playGame() {
+    let gameRounds = 5;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (gameRounds > 0) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        const result = playRound(humanChoice, computerChoice);
+        if (result === 1) {
+            humanScore++;
+        } else if (result === 0) {
+            computerScore++;
+        } else {
+            gameRounds++;
+        }
+        gameRounds--;
+        console.log(`Rounds left: ${gameRounds}. Your score: ${humanScore}. Computer score: ${computerScore}.`);
+    }
+    
+    if (humanScore > computerScore) {
+        console.log(`You won! Your score: ${humanScore}. Computer score: ${computerScore}.`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lost! Your score: ${humanScore}. Computer score: ${computerScore}.`);
+    } else {
+        console.log("It's a tie! Your score: ${");
+    }
+
 }

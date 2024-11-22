d96be867be3b57fc5074e233c60d284178e1ca03
ahgr3y <leehh97@gmail.com>
2024-11-22 11:51:11 +0800

    Add function to get computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 1) {
+        return "rock";
+    } else if (randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
